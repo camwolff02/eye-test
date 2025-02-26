ef01f694e1cff0e32de938636f05878825582d5d
cam <cameron.wolff@proton.me>
2025-02-26 00:05:43 -0800

    added servo code, untested

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,6 +4,10 @@
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
+use esp_hal::gpio::IO;
+use esp_hal::ledc::{channel, timer, LSGlobalClkSource, Ledc};
+use esp_hal::peripherals::Peripherals;
+use esp_hal::prelude::*;
 use esp_hal::timer::timg::TimerGroup;
 
 #[panic_handler]
@@ -13,20 +17,44 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    // generator version: 0.3.0
-
+    // Initialize peripherals
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
-
     let timer0 = TimerGroup::new(peripherals.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    // Setup GPIO and PWM for servo control
+    let io = IO::new(peripherals.GPIO);
+    let mut ledc = Ledc::new(peripherals.LEDC, &mut peripherals.SYSTEM);
+    let mut timer = timer::LedcTimer::new(
+        &mut ledc,
+        timer::Number::Timer0,
+        &timer::config::Config {
+            duty: timer::config::Duty::Duty10Bit,
+            clock_source: LSGlobalClkSource::APBClk,
+            frequency: 50_u32.kHz().into(), // 50Hz for servos
+        },
+    );
 
-    loop {
-        Timer::after(Duration::from_secs(1)).await;
+    let mut pwm_channel = channel::LedcChannel::new(
+        &mut ledc,
+        channel::Number::Channel0,
+        &timer,
+        io.pins.gpio4.into_push_pull_output(), // Change pin as needed
+    );
+
+    // Function to set servo position
+    async fn set_servo_position(channel: &mut channel::LedcChannel, position: u16) {
+        let duty = (position as u32 * 1024 / 180) + 26; // Mapping position to duty cycle
+        channel.set_duty(duty.min(1023));
     }
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
+    loop {
+        set_servo_position(&mut pwm_channel, 0).await;
+        Timer::after(Duration::from_secs(1)).await;
+        set_servo_position(&mut pwm_channel, 90).await;
+        Timer::after(Duration::from_secs(1)).await;
+        set_servo_position(&mut pwm_channel, 180).await;
+        Timer::after(Duration::from_secs(1)).await;
+    }
 }
